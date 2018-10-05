35455acfa3a29953778efbbbc5fef0770db638d1
Taras <taraskim@wknown.com>
2018-10-05 12:39:30 -0400

    Fixes

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -17,34 +17,21 @@
     <!-- Assets -->
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'/>
 
-    <link rel="stylesheet" href="{!! asset('css/tilda-grid-3.0.min.css') !!}" type="text/css" media="all" />
-    <link rel="stylesheet" href="{!! asset('css/tilda-blocks-2.12.css') !!}" type="text/css" media="all" />
-    <link rel="stylesheet" href="{!! asset('css/tilda-animation-1.0.min.css') !!}" type="text/css" media="all" />
-    <link rel="stylesheet" href="{!! asset('css/tilda-slds-1.4.min.css') !!}" type="text/css" media="all" />
-    <link rel="stylesheet" href="{!! asset('css/tilda-zoom-2.0.min.css') !!}" type="text/css" media="all" />
-    <link rel="stylesheet" href="{!! asset('css/tooltipster.min.css') !!}" type="text/css" media="all" />
-    <link rel="stylesheet" href="https://static.tildacdn.com/css/tilda-grid-3.0.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="https://project80487.tilda.ws/tilda-blocks-2.12.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="https://static.tildacdn.com/css/tilda-animation-1.0.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="https://static.tildacdn.com/css/tilda-slds-1.4.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="https://static.tildacdn.com/css/tilda-zoom-2.0.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="https://static.tildacdn.com/css/tilda-popup-1.1.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="https://static.tildacdn.com/css/tooltipster.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="css/tilda-grid-3.0.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/tilda-blocks-2.12.css?t=1538552615" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/tilda-animation-1.0.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/tilda-slds-1.4.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/tilda-zoom-2.0.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/tilda-popup-1.1.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/tooltipster.min.css" type="text/css" media="all" />
+
+
     <!-- Template Custom CSS -->
     <link rel="stylesheet" href="{!! asset('css/custom.css') !!}" type="text/css" media="all">
-    <script src="https://static.tildacdn.com/js/jquery-1.10.2.min.js"></script>
-    <script src="https://static.tildacdn.com/js/tilda-scripts-2.8.min.js"></script>
-    <script src="https://project80487.tilda.ws/tilda-blocks-2.7.js"></script>
-    <script src="https://static.tildacdn.com/js/lazyload-1.3.min.js"></script>
-    <script src="https://static.tildacdn.com/js/tilda-animation-1.0.min.js"></script>
-    <script src="https://static.tildacdn.com/js/tilda-slds-1.4.min.js"></script>
-    <script src="https://static.tildacdn.com/js/hammer.min.js"></script>
-    <script src="https://static.tildacdn.com/js/tilda-zoom-2.0.min.js"></script>
-    <script src="https://static.tildacdn.com/js/typed.min.js"></script>
-    <script src="https://static.tildacdn.com/js/jquery.tooltipster.min.js"></script>
+
     <script src="{!! asset('js/jquery-1.10.2.min.js') !!}"></script>
     <script src="{!! asset('js/tilda-scripts-2.8.min.js') !!}"></script>
-    <script src="{!! asset('js/tilda-blocks-2.7.js') !!}"></script>
+    <script src="{!! asset('js/tilda-blocks-2.7.js?t=1538552615') !!}"></script>
     <script src="{!! asset('js/lazyload-1.3.min.js') !!}"></script>
     <script src="{!! asset('js/tilda-animation-1.0.min.js') !!}"></script>
     <script src="{!! asset('js/typed.min.js') !!}"></script>
